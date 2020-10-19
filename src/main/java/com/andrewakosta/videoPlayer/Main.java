fb432c736acc634f50d304476209d697bb2b9755
Andres Acosta <andresakosta@outlook.com>
2020-10-19 18:32:57 -0500

    view Select the Directory added

--- src/main/java/com/andrewakosta/videoPlayer/Main.java
+++ src/main/java/com/andrewakosta/videoPlayer/Main.java
@@ -1,13 +1,20 @@
 package com.andrewakosta.videoPlayer;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
@@ -16,8 +23,19 @@ public class Main extends Application {
         launch(args);
     }
 
+    @FXML
+    private TextField seeker;
+
+    @FXML
+    private GridPane gridPaneMain;
+
     @Override
     public void start(Stage primaryStage) {
+
+        seeker = new TextField();
+        gridPaneMain = new GridPane();
+
+
         Parent root  = null;
         try{
             root = FXMLLoader.load(getClass().getResource("main.fxml"));
@@ -28,6 +46,26 @@ public class Main extends Application {
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
     }
 
+
+    public void handleBtnSearchVideos(MouseEvent mouseEvent) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select Videos Folder");
+        Stage stage = new Stage();
+        File file = directoryChooser.showDialog(stage);
+
+        if(file != null ){
+            System.out.println(file.getPath());
+            seeker.setText(file.getPath());
+        }else {
+            System.out.println("Something was wrong");
+        }
+    }
+
+    public void goToVideosView(MouseEvent mouseEvent) {
+        gridPaneMain.getChildren().clear();
+    }
 }
